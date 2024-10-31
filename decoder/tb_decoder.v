f7c10003254f7797f3b2a53ac587e1763cb46071
Drizzle <drizzleluo876@gmail.com>
2024-10-31 23:29:06 +0800

    完成作业

--- decoder/tb_decoder.v
+++ decoder/tb_decoder.v
@@ -3,13 +3,13 @@ module tb_decoder;
 reg [2:0] in;
 wire [7:0] out;
 
-decoder uut (  // 实例化被测试的译码器模块
+decoder uut (
     .in(in),
     .out(out)
 );
 
 initial begin
-    // 生成激励信号进行测试
+
     in = 3'b000; #10;
     in = 3'b001; #10;
     in = 3'b010; #10;
@@ -18,7 +18,7 @@ initial begin
     in = 3'b101; #10;
     in = 3'b110; #10;
     in = 3'b111; #10;
-    $stop;  // 结束仿真
+    $stop;
 end
 
 endmodule
